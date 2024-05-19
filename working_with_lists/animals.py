7295ca18e14d2506dc96a1ceb8b46b52dd105172
Joe Adorno <joseph.adorno.3@gmail.com>
2024-05-19 13:20:47 -0500

    adding two new files to the repo

---
+++ working_with_lists/animals.py
@@ -0,0 +1,11 @@
+ # this is a project to create a simple list with animals and print out some statements about them to practice for loops
+
+animals = ['cat', 'dog', 'fish']
+
+for animal in animals: 
+	print(animal)
+
+for animal in animals:
+	print(f"\nA {animal} would make a great pet!")
+
+print("\nThese are all great animals!")
---
+++ working_with_lists/pizzas.py
@@ -0,0 +1,12 @@
+# this is an exercise to write up a list of different pizzas I enjoy in a list and then print the output
+
+pizza_list = ['new york style', 'deep dish', 'tavern style']
+
+for pizza in pizza_list: 
+    print(pizza)
+
+for pizza in pizza_list: 
+    print(f"I really love eating {pizza} pizza!")
+
+print("\nThere are so many great kinds of pizza!")
+
